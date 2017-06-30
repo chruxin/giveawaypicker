0caa0444e56df35026ad2da39271c3a113bcbb33
Mandy Chen <42mandychen@users.noreply.github.com>
2017-06-29 19:55:21 -0700

    Use reactstrap for navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,53 +1,38 @@
 import React, { Component } from 'react';
-import { Button } from 'reactstrap';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 class NavBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
     return (
-      <div>
-        <div className="bg-faded">
-          <div className="container mt-0">
-            <nav className="navbar navbar-toggleable-sm navbar-light">
-              <a className="navbar-brand" href="http://www.mandychen.me">
-                &#60;MANDY CHEN /&#62;
-              </a>
-              <button
-                className="navbar-toggler navbar-toggler-right"
-                type="button" data-toggle="collapse"
-                data-target="#bd-main-nav"
-                aria-controls="bd-main-nav"
-                aria-expanded="false"
-              aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-
-
-              <div className="collapse navbar-collapse" id="bd-main-nav">
-                <ul className="nav navbar-nav mr-right">
-                  <li className="nav-item">
-                    <a className="nav-item nav-link" href="#">INSTAGRAM <span className="sr-only"></span></a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-item nav-link" href="#">FACEBOOK</a>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-          </div>
-        </div>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">INSTAGRAM <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">FACEBOOK</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled" href="#">Disabled</a>
-            </li>
-          </ul>
+      <div className="bg-faded">
+        <div className="container">
+          <Navbar color="faded" light toggleable>
+            <NavbarToggler right onClick={this.toggle} />
+            <NavbarBrand href="/">Giveaway Picker</NavbarBrand>
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink href="/components/">INSTAGRAM</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://github.com/reactstrap/reactstrap">FACEBOOK</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
         </div>
       </div>
     );
