8ad3f492942e7af4ae9958b457f6626a4b2fed01
Mandy Chen <42mandychen@users.noreply.github.com>
2017-07-11 17:16:16 -0700

    Conditional rendering template for facebook page

--- src/App.js
+++ src/App.js
@@ -50,9 +50,17 @@ class Instagram extends Component {
 
 class Facebook extends Component {
   render () {
+    const loggedIn = false;
+    let heading = null;
+    if (loggedIn) {
+      heading = <h1>Facebook</h1>;
+    } else {
+      heading = <h1>Facebook - Please log in</h1>;
+    }
+
     return (
       <div className="text-center">
-        <h1>Facebook</h1>
+        { heading }
       </div>
     );
   }
